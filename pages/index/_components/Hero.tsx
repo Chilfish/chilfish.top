137f2f5b6dd03d608b7b8771aa6d89f2ed214cad
Chill Fish <chill4fish@gmail.com>
2023-03-15 00:44:40 +0800

    03-15

--- pages/index/_components/Hero.tsx
+++ pages/index/_components/Hero.tsx
@@ -1,7 +1,7 @@
 export default () => {
   return (
     <div>
-      <img class="w-35 sm:w-55 rounded-2xl" src="/Avatar.jpg" alt="Avatar" />
+      <img class="w-35 sm:w-55 rounded-2xl" src="/Avatar.webp" alt="Avatar" />
     </div>
   );
 };
