2f792d56a2f7f58672ef44f2c2265cf4627d3cca
Chilfish <chill4fish@gmail.com>
2024-02-07 14:23:17 +0800

    feat: add comments

---
+++ src/components/post/Comments.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Giscus from '@giscus/vue'
+</script>
+
+<template>
+  <div class="mt-12">
+    <Giscus
+      repo="chilfish/chilfish.top"
+      repo-id="R_kgDOJInwOA"
+      category="General"
+      category-id="DIC_kwDOJInwOM4CdAZu"
+      mapping="title"
+      strict="0"
+      reactions-enabled="1"
+      emit-metadata="0"
+      input-position="top"
+      theme="preferred_color_scheme"
+      lang="zh-CN"
+      loading="lazy"
+      crossorigin="anonymous"
+      async
+    />
+  </div>
+</template>
