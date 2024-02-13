4fb666a045e7d836caf3ead490c6848a2cb2d9f6
Chilfish <chill4fish@gmail.com>
2024-02-13 15:01:29 +0800

    fix: builds

--- src/env.d.ts
+++ src/env.d.ts
@@ -11,32 +11,3 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
-
-declare module 'rehype-figure' {
-
-  const rehypeFigure: () => any
-  export default rehypeFigure
-}
-
-declare module 'sanitize-html' {
-  export default function sanitizeHtml(
-    html: string,
-    options?: Record<string, unknown>,
-  ): string
-}
-
-declare module 'markdown-it' {
-  import type { PluginSimple } from 'markdown-it/lib'
-  import type { Options } from 'markdown-it/lib'
-
-  interface MarkdownIt {
-    use(plugin: PluginSimple, ...params: unknown[]): this
-    render(md: string): string
-  }
-
-  const MarkdownIt: {
-    new (options?: Options): MarkdownIt
-  }
-
-  export default MarkdownIt
-}
